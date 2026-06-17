93a94b33dfa92d8bec673161e3fd148751ad2549
TristenD706 <tristen.davis@mail.mcgill.ca>
2026-06-17 15:27:00 -0400

    Add files via upload
    updated UI that allows for multiple glands and tumors, will remove past circles/points if the add button is pressed again for each type, and unit conversions are now done in the .mlapp so they will already be in meters as the output of the UI app.

--- positionFromPhoto.m
+++ positionFromPhoto.m
@@ -1,33 +1,13 @@
-function positionFromPhoto(phantomCenter,phantomRadius,tumorCenter,tumorRadius,glandCenter,glandRadius,antennaPositions,physicalRadius)
-
-    radiusMeters = physicalRadius/100;
-    scale = radiusMeters/phantomRadius;
-    
-    tumorRadiusMeters = tumorRadius*scale;
-    glandRadiusMeters = glandRadius*scale;
-    phantomCenterMeters = phantomCenter*scale;
-    phantomCenterMeters(2) = -phantomCenterMeters(2);
-    tumorCenterMeters = tumorCenter*scale;
-    tumorCenterMeters(2) = -tumorCenterMeters(2);
-    glandCenterMeters = glandCenter*scale;
-    glandCenterMeters(2) = -glandCenterMeters(2);
-    antennaPositionsMeters = antennaPositions*scale;
-    antennaPositionsMeters(:,2) = -antennaPositionsMeters(:,2);
-    tumorCenterMeters = tumorCenterMeters - phantomCenterMeters;
-    glandCenterMeters = glandCenterMeters - phantomCenterMeters;
-    antennaPositionsMeters = antennaPositionsMeters - phantomCenterMeters;
-    phantomCenterMeters = phantomCenterMeters - phantomCenterMeters;
-    
-    antennaPositionsMeters
-
+function positionFromPhoto(phantomCenterMeters,radiusMeters,tumorCentersMeters,tumorRadiiMeters,glandCentersMeters,glandRadiiMeters,antennaPositionsMeters)
+   
     figure
     hold on
     viscircles(phantomCenterMeters, radiusMeters, 'Color', 'r')
-    viscircles(tumorCenterMeters, tumorRadiusMeters, 'Color', 'y')
-    viscircles(glandCenterMeters, glandRadiusMeters, 'Color', 'g')
+    viscircles(tumorCentersMeters, tumorRadiiMeters, 'Color', 'y')
+    viscircles(glandCentersMeters, glandRadiiMeters, 'Color', 'g')
     scatter(antennaPositionsMeters(:,1),antennaPositionsMeters(:,2),'filled','blue')
-    scatter(tumorCenterMeters(:,1),tumorCenterMeters(:,2),'filled','yellow')
-    scatter(glandCenterMeters(:,1),glandCenterMeters(:,2),'filled','green')
+    scatter(tumorCentersMeters(:,1),tumorCentersMeters(:,2),'filled','yellow')
+    scatter(glandCentersMeters(:,1),glandCentersMeters(:,2),'filled','green')
     scatter(phantomCenterMeters(:,1),phantomCenterMeters(:,2),'filled','red')
     axis square
 
